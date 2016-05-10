f59a9ac3720c08576c468f26e5a1797b0abe2e75
Dale Emery <dale@dhemery.com>
2016-05-10 12:38:02 -0700

    md2tex writes the content of a md comment

--- lib/dbp/book_compiler/markdown_to_tex/translator.rb
+++ lib/dbp/book_compiler/markdown_to_tex/translator.rb
@@ -1,7 +1,8 @@
 module DBP::BookCompiler::MarkdownToTex
   class Translator
     def translate(reader, writer)
-
+      reader.scan(/<!--[[:space:]]*(.*?)[[:space:]]*-->/)
+      writer.write(reader[1])
     end
   end
 end
