0782a55592c08a65f67062bb1c2d21bc2773d70a
Dale Emery <dale@dhemery.com>
2016-05-26 15:02:40 -0700

    Tokens are method name symbols mapped to patterns

--- lib/dbp/book_compiler/markdown_to_tex/translator.rb
+++ lib/dbp/book_compiler/markdown_to_tex/translator.rb
@@ -1,83 +1,69 @@
 module DBP::BookCompiler::MarkdownToTex
   class Translator
-    BODY_TEXT = {
-        pattern: /([^<*_]+)/,
-        command: :write
+    TOKENS = {
+        # text with no operators -> copy
+        copy: /([^<*_]+)/,
+
+        # div.class -> enter the environment named by .class
+        enter: /<div\s+class\s*=\s*"\s*([^[:space:]]+)\s*"\s*>/,
+
+        # any end tag -> exit the current environment or macro
+        exit: /<\/.*?>/,
+
+        # comment -> extract the content
+        extract: /<!--\s*(.*?)\s*-->/,
+
+        # void tag (currently only <br/>) -> replace with text from REPLACEMENTS
+        replace: /<(br)\s*\/>/,
+
+        # any other text -> fail with a diagnostic
+        reject: /(.{1,80})/,
     }
 
-    BR_TAG = {
-        pattern: /<(br)\s*\/>/,
-        command: :replace
-    }
-
-    COMMENT_CONTENT = {
-        pattern: /<!--\s*(.*?)\s*-->/,
-        command: :write
-    }
-
-    DIV_START_TAG = {
-        pattern: /<div\s+class\s*=\s*"\s*([^[:space:]]+)\s*"\s*>/,
-        command: :enter_environment
-    }
-
-    END_TAG = {
-        pattern: /<\/.*?>/,
-        command: :pop
-    }
-
-    # Matches any text and raises an error.
-    # Match against this last to catch otherwise unmatched pattern.
-    UNRECOGNIZED = {
-        pattern: /(.{1,80})/,
-        command: :fail
-    }
-
-    TOKENS = [
-        BODY_TEXT,
-        BR_TAG,
-        COMMENT_CONTENT,
-        DIV_START_TAG,
-        END_TAG,
-        UNRECOGNIZED,
-    ]
-
-    REPLACEMENT = {
+    REPLACEMENTS = {
         'br' => '\break '
     }
 
-    def initialize(scanner, writer, tokens = TOKENS)
+    def initialize(scanner, writer)
       @scanner = scanner
       @writer = writer
-      @tokens = tokens
       @stack = []
     end
 
     def translate
       until @scanner.eos? do
-        token = @tokens.find { |token| @scanner.scan(token[:pattern]) }
-        self.send(token[:command], @scanner[1])
+        token = TOKENS.each_key.find { |token| @scanner.scan(TOKENS[token]) }
+        self.send(token, @scanner[1])
       end
     end
 
-    def enter_environment(name)
+    def copy(text)
+      write(text)
+    end
+
+    def enter(name)
       write "\\begin{#{name}}"
       push "\\end{#{name}}"
     end
 
-    def fail(text)
+    def exit(_)
+      write @stack.pop
+    end
+
+    def extract(comment)
+      write comment
+    end
+
+    def reject(text)
       raise "Unrecognized text starting with: #{text}"
     end
 
-    def pop(_)
-      write @stack.pop
+    def replace(text)
+      write REPLACEMENTS[text]
     end
 
     def push(text)
       @stack.push text
-    end
-
-    def replace(text)
-      write REPLACEMENT[text]
     end
 
     def write(text)
--- spec/dbp/md2tex/tokens_spec.rb
+++ spec/dbp/md2tex/tokens_spec.rb
@@ -2,135 +2,103 @@ require_relative '_helper'
 require 'dbp/book_compiler/markdown_to_tex/translator'
 
 module DBP::BookCompiler::MarkdownToTex
-  describe Translator, 'tokens' do
-    CAPTURED_TEXT = 'some capture'
+  describe Translator, 'token patterns' do
+    let(:scanner) { StringScanner.new(input) }
+    let(:pattern) { Translator::TOKENS[subject] }
 
-    describe 'BODY_TEXT' do
-      subject { Translator::BODY_TEXT }
-      let(:translator) { MiniTest::Mock.new }
+    before do
+      scanner.scan(pattern)
+    end
 
-      describe :pattern do
-        describe 'against a string with no operator characters' do
-          let(:scanner) { StringScanner.new(input) }
-          let(:input) { 'a string with no operator characters!@#$%&()-+=' }
+    describe :copy do
+      subject { :copy}
 
-          it 'matches the entire string' do
-            scanner.scan(subject[:pattern])
+      describe 'against a string with no operator characters' do
+        let(:input) { 'a string with no operator characters!@#$%&()-+=' }
 
-            _(scanner.matched).must_equal input
+        it 'matches the entire string' do
+          _(scanner.matched).must_equal input
+        end
+
+        it 'captures the entire match' do
+          _(scanner[1]).must_equal scanner.matched
+        end
+      end
+
+      %w{< * _}.each do |c|
+        describe "against text followed by an operator character #{c}" do
+          let(:text) { 'some text' }
+          let(:input) { text + c + 'additional text' }
+
+          it 'stops matching at the operator character' do
+            _(scanner.matched).must_equal text
           end
 
           it 'captures the entire match' do
-            subject[:pattern] =~ input
-
             _(scanner[1]).must_equal scanner.matched
           end
         end
+      end
+    end
 
-        %w{< * _}.each do |c|
-          describe "against a string with operator character #{c}" do
-            let(:body_text) { 'before the operator character' }
-            let(:input) { body_text + c + 'additional text' }
-            let(:scanner) { StringScanner.new(input) }
+    describe :enter do
+      subject { :enter }
 
-            it 'stops matching at the operator character' do
-              scanner.scan(subject[:pattern])
+      describe 'against a div tag with a class attribute' do
+        let(:class_attribute_value) { '     monkey     ' }
+        let(:tag) { %Q{<div                    class    =        "#{class_attribute_value}"              >} }
+        let(:input) { tag + 'additional text' }
 
-              _(scanner.matched).must_equal body_text
-            end
+        it 'matches the tag' do
+          _(scanner.matched).must_equal tag
+        end
 
-            it 'captures the entire match' do
-              scanner.scan(subject[:pattern])
-
-              _(scanner[1]).must_equal scanner.matched
-            end
-          end
+        it 'captures the stripped class attribute value' do
+          _(scanner[1]).must_equal class_attribute_value.strip
         end
       end
     end
 
-    describe 'COMMENT_CONTENT' do
-      subject { Translator::COMMENT_CONTENT }
+    describe :exit do
+      subject { :exit}
 
-      describe :pattern do
-        describe 'against a string with a comment' do
-          let(:content) { '                   some comment            ' }
-          let(:comment) { '<!--' + content + '-->' }
-          let(:input) { comment + 'additional text' }
-          let(:scanner) { StringScanner.new(input) }
+      describe 'against a string with any end tag' do
+        let(:tag) { '</                      monkeymonkey               >' }
+        let(:input) { tag + 'additional text' }
 
-          it 'matches the comment' do
-            scanner.scan(subject[:pattern])
-
-            _(scanner.matched).must_equal comment
-          end
-
-          it 'captures the stripped comment content' do
-            scanner.scan(subject[:pattern])
-
-            _(scanner[1]).must_equal content.strip
-          end
+        it 'matches the tag' do
+          _(scanner.matched).must_equal tag
         end
       end
     end
 
-    describe 'BR_TAG' do
-      subject { Translator::BR_TAG }
+    describe :extract do
+      subject { :extract }
 
-      describe :pattern do
-        describe 'against a string with a BR tag' do
-          let(:tag) { '<br        />' }
-          let(:input) { tag + 'additional text' }
-          let(:scanner) { StringScanner.new(input) }
+      describe 'against a comment' do
+        let(:content) { '                   some comment            ' }
+        let(:comment) { "<!--#{content}-->" }
+        let(:input) { "#{comment}additional text" }
 
-          it 'matches the tag' do
-            scanner.scan(subject[:pattern])
+        it 'matches the comment' do
+          _(scanner.matched).must_equal comment
+        end
 
-            _(scanner.matched).must_equal tag
-          end
+        it 'captures the stripped comment content' do
+          _(scanner[1]).must_equal content.strip
         end
       end
     end
 
-    describe 'DIV_START_TAG' do
-      subject { Translator::DIV_START_TAG }
+    describe :replace do
+      subject { :replace }
 
-      describe :pattern do
-        describe 'against a div tag with a class attribute' do
-          let(:class_attribute_value) { '     monkey     ' }
-          let(:tag) { %Q{<div                    class    =        "#{class_attribute_value}"              >} }
-          let(:input) { tag + 'additional text' }
-          let(:scanner) { StringScanner.new(input) }
+      describe 'against a string with a BR tag' do
+        let(:tag) { '<br        />' }
+        let(:input) { tag + 'additional text' }
 
-          it 'matches the tag' do
-            scanner.scan(subject[:pattern])
-
-            _(scanner.matched).must_equal tag
-          end
-
-          it 'captures the stripped class attribute value' do
-            scanner.scan(subject[:pattern])
-
-            _(scanner[1]).must_equal class_attribute_value.strip
-          end
-        end
-      end
-    end
-
-    describe 'END_TAG' do
-      subject { Translator::END_TAG }
-
-      describe :pattern do
-        describe 'against a string with any end tag' do
-          let(:tag) { '</                      monkeymonkey               >' }
-          let(:input) { tag + 'additional text' }
-          let(:scanner) { StringScanner.new(input) }
-
-          it 'matches the tag' do
-            scanner.scan(subject[:pattern])
-
-            _(scanner.matched).must_equal tag
-          end
+        it 'matches the tag' do
+          _(scanner.matched).must_equal tag
         end
       end
     end
